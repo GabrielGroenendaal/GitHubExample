3ea9e4e10882dd79d272c815600c9985da788d69
Gabriel Groenendaal <gfg243@nyu.edu>
2019-02-21 12:15:43 -0500

    Uploading to Bop

---
+++ 3d movement/Assets/SphereControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody thisRigid;
+    public Camera camera;
+
+    public float speed;
+    public Vector2 torque;
+    
+    public 
+    
+    void Start()
+    {
+        thisRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
